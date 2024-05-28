6f8623dca9580090c4e877d583b6e4e9f476e30e
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-05-28 16:11:02 -0400

    Add click event listener to restaurant options

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const diningBtn = document.getElementById('diningBtn')
   const activityBtn = document.getElementById('activityBtn')
   const sightsBtn = document.getElementById('sightsBtn')
+  const optionDetails = document.getElementById('optionDetails')
 
     function displayDiningOptions() {
       let optionsVisible = false
@@ -35,12 +36,44 @@ document.addEventListener('DOMContentLoaded', function () {
               div.appendChild(h2)
               li.appendChild(div)
               diningOptions.appendChild(li)
+
+              img.addEventListener('click', (e) => {
+                e.preventDefault()
+
+                let detailsContainer = document.createElement('div')
+                let optionName = document.createElement('h2')
+                let img2 = document.createElement('img')
+                let img3 = document.createElement('img')
+                let description = document.createElement('p')
+
+                optionName.innerText = restaurant.name
+                img2.src = restaurant.images[1]
+                img3.src = restaurant.images[2]
+                description.innerText = restaurant.description
+
+                detailsContainer.appendChild(optionName)
+                detailsContainer.appendChild(img2)
+                detailsContainer.appendChild(img3)
+                detailsContainer.appendChild(description)
+
+                optionDetails.appendChild(detailsContainer)
+              })
             })
             optionsVisible = true
           })
         }
       })
     }
+
+    //Create on click fucntion for each option card
+    //Create details card
+    // Img2 Img3, h2 name, p description
+    //On click will display details of the option
+    //Details will include Location, Description, other Imgs
+
+
+
+
 
     function displayActivityOptions() {
       let optionsVisible = false
