211ad3aa86812055db0ad4bd57acb96829f02891
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-05-28 13:33:05 -0400

    Test fetch request on new database

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 console.log('hello world')
+
+document.addEventListener('DOMContentLoaded', function () {
+  const allRestaurantsURL = 'http://localhost:3000/restaurants'
+  const allSitesURL = 'http://localhost:3000/sites'
+  const allActivitiesURL = 'http://localhost:3000/activities'
+
+
+
+    fetch(allRestaurantsURL)
+    .then(res => res.json())
+    .then(console.log)
+
+    fetch(allSitesURL)
+    .then(res => res.json())
+    .then(console.log)
+
+    fetch(allActivitiesURL)
+    .then(res => res.json())
+    .then(console.log)
+
+
+})
